8ecd3eae19f63a2ddd66dfbc113669a33daba857
Ahmed HILALI <ahmedhilali07@gmail.com>
2021-10-08 23:23:41 +0100

    fix login page

--- serverSide/ConnectionDataBase.php
+++ serverSide/ConnectionDataBase.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password,$db);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$result = mysqli_query($conn, "SELECT * From  rod_all_data"); 
+$result = mysqli_query($conn, "SELECT 'DIVISION' From  rod_all_data");
 $rows = array();
 
           while($row = mysqli_fetch_array($result))
