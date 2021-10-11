f47c46f28a3211c7a3a9bbd00c16f0a3f50a35ff
Mastour <mouhcine4mastour@gmail.com>
2021-10-11 18:27:36 +0000

    Select*

--- serverSide/ConnectionDataBase_001.php
+++ serverSide/ConnectionDataBase_001.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password,$db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$result = mysqli_query($conn, "SELECT 'DIVISION' From  rod_all_data");
+$result = mysqli_query($conn, "SELECT * From  rod_all_data");
 $rows = array();
 
 while($row = mysqli_fetch_array($result))
@@ -21,18 +21,5 @@ while($row = mysqli_fetch_array($result))
 }
 echo json_encode($rows);
 
-
-
-
-/*   if ($result->num_rows > 0) { */
-// output data of each row
-/* while($row = $result->fetch_assoc()) { */
-/* echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. "<br>"; */
-/*  $rows[] = $row;
-}
-echo json_encode($rows);
-} else {
-echo "0 results";
-} */
 $conn->close();
 ?>
