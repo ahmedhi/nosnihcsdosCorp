2e76c8ef853cfb66344a08c90248ee6e75bdb4ae
elaraki <anas_iraki@hotmail.com>
2021-11-24 18:05:35 +0100

    ajouter nouvelle source

--- .history/serverSide/RedirectPageMail_20211109161528.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $python_print = "scriptsitelogin.py"; 
-    $python_execution = "python ".$python_print; 
-    $output= shell_exec($python_execution);
-     echo $output;
-?>
--- .history/serverSide/verification_20211104095821.py
+++
@@ -1,2 +0,0 @@
-
-print("Vérification complete")
